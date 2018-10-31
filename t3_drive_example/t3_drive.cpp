8882d2102c7858a7a16667f91db16a28a08a0a4a
Xinxing <cxx@hust.edu.cn>
2018-10-31 10:23:28 +0800

    Fix a bug of buffer size

--- t3_drive_example/t3_drive.cpp
+++ t3_drive_example/t3_drive.cpp
@@ -21,7 +21,7 @@ int vel_set() {
     
     int i;
     int Ret;
-    char Buffer[20];
+    char Buffer[30];
     
     Ret=LS.WriteString("enable 1\n");                                             // Send the command on the serial port
     if (Ret!=1) {                                                           // If the writting operation failed ...
@@ -49,7 +49,7 @@ int vel_set() {
     printf ("Write operation is successful \n");
 
     // Read a string from the serial device
-    Ret=LS.ReadString(Buffer,'\n',128,10000);                                // Read a maximum of 128 characters with a timeout of 5 seconds                                                                                   // The final character of the string must be a line feed ('\n')
+    Ret=LS.ReadString(Buffer,'\n',128,100000);                                // Read a maximum of 128 characters with a timeout of 5 seconds                                                                                   // The final character of the string must be a line feed ('\n')
     if (Ret>0)      
         
         // If a string has been read from, print the string
@@ -58,8 +58,9 @@ int vel_set() {
         printf ("TimeOut reached. No data received !\n");                   // If not, print a message.
     
     usleep(1000);
+    
 
-    Ret=LS.WriteString("set_vel 5 5\n");                                             // Send the command on the serial port
+    Ret=LS.WriteString("set_vel 50 50\n");                                             // Send the command on the serial port
     if (Ret!=1) {                                                           // If the writting operation failed ...
         printf ("Error while writing data\n");                              // ... display a message ...
         return Ret;                                                         // ... quit the application.
@@ -75,7 +76,7 @@ int vel_set() {
     else
         printf ("TimeOut reached. No data received !\n");                   // If not, print a message.
     
-    usleep(10000000);
+    usleep(1000000);
 
     Ret=LS.WriteString("set_vel 0 0\n");                                             // Send the command on the serial port
     if (Ret!=1) {                                                           // If the writting operation failed ...
